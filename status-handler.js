44aabe9ac3f18adf3b44dd301e1afc3b6eceeb5d
Astrolic <ericantebo@live.se>
2021-08-06 15:00:51 +0000

    Add javascript that helps display bot status

---
+++ status-handler.js
@@ -0,0 +1,19 @@
+var url = "https://miner.minerbot.repl.co/api/status"
+    
+const http = new XMLHttpRequest();
+http.open("GET", url);
+http.send();
+
+http.onreadystatechange = (e) => {
+    
+    var status = this.document.getElementById("status")
+
+    if (http.status == 200){
+        status.innerHTML = "Online"
+        status.style.color = "#12a600"
+    } else{
+        status.innerHTML = "Offline"
+        status.style.color = "#b80000"
+    }
+
+}
